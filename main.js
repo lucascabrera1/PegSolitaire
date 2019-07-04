3a58bbc527b8dfd9fe05853f31425b83ae480996
= <lucas_cabrera1@hotmail.com>
2019-07-04 10:53:34 -0300

    Add initial board

---
+++ main.js
@@ -0,0 +1,9 @@
+const initialState = [
+  [undefined, undefined, 1, 1, 1, undefined, undefined],
+  [undefined, undefined, 1, 1, 1, undefined, undefined],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 0, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [undefined, undefined, 1, 1, 1, undefined, undefined],
+  [undefined, undefined, 1, 1, 1, undefined, undefined],
+];
