ef50670dd516a2133af98c09749238fefa69d2b6
= <lucas_cabrera1@hotmail.com>
2019-07-04 11:37:11 -0300

    Add logic

--- main.js
+++ main.js
@@ -7,3 +7,27 @@ const initialState = [
   [undefined, undefined, 1, 1, 1, undefined, undefined],
   [undefined, undefined, 1, 1, 1, undefined, undefined],
 ];
+
+let dynamicBoard = '<ul>';
+for (let i=0; i< initialState.length; i++)
+{
+  dynamicBoard = '<li>'
+  for (let j=0; j< initialState[i].length; j++)
+  {
+    dynamicBoard += '<button></button>';
+  }
+  dynamicBoard += '</li>'
+}
+dynamicBoard += '</ul>'
+
+const boardElement = document.getElementById('board');
+
+//Asignamos dynamicBoard
+boardElement.innerHTML = dynamicBoard;
+
+console.log(dynamicBoard);
+window.onload = function()
+{
+  const boardElement = document.getElementById('board');
+  boardElement.innerHTML = dynamicBoard;
+}
